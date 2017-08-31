4181d5751609bcf824da76e0367d44180ad06b71
Aracher17 <die.drei99@yahoo.de>
2017-08-31 15:57:09 +0200

    Update AhoyOnboarderActivity.java
    Added Skip Button & onPageChangeListener

--- onboarder/src/main/java/com/codemybrainsout/onboarder/AhoyOnboarderActivity.java
+++ onboarder/src/main/java/com/codemybrainsout/onboarder/AhoyOnboarderActivity.java
@@ -33,7 +33,7 @@ public abstract class AhoyOnboarderActivity extends AppCompatActivity implements
     private CircleIndicatorView circleIndicatorView;
     private ViewPager vpOnboarderPager;
     private AhoyOnboarderAdapter ahoyOnboarderAdapter;
-    private TextView btnSkip;
+    private TextView btnSkip,textViewSkipOnBoarding;
     private ImageView ivNext, ivPrev;
     private FrameLayout navigationControls;
     private FrameLayout buttonsLayout;
@@ -57,6 +57,7 @@ public abstract class AhoyOnboarderActivity extends AppCompatActivity implements
         parentLayout = (RelativeLayout) findViewById(R.id.parent_layout);
         circleIndicatorView = (CircleIndicatorView) findViewById(R.id.circle_indicator_view);
         btnSkip = (TextView) findViewById(R.id.btn_skip);
+        textViewSkipOnBoarding = (TextView) findViewById(R.id.textViewSkip);
         buttonsLayout = (FrameLayout) findViewById(R.id.buttons_layout);
         navigationControls = (FrameLayout) findViewById(R.id.navigation_layout);
         ivNext = (ImageView) findViewById(R.id.ivNext);
@@ -65,6 +66,7 @@ public abstract class AhoyOnboarderActivity extends AppCompatActivity implements
         backgroundImageOverlay = (View) findViewById(R.id.background_image_overlay);
         vpOnboarderPager = (ViewPager) findViewById(R.id.vp_pager);
         vpOnboarderPager.addOnPageChangeListener(this);
+        textViewSkipOnBoarding.setOnClickListener(this);
         btnSkip.setOnClickListener(this);
         ivPrev.setOnClickListener(this);
         ivNext.setOnClickListener(this);
@@ -110,6 +112,8 @@ public abstract class AhoyOnboarderActivity extends AppCompatActivity implements
             vpOnboarderPager.setCurrentItem(vpOnboarderPager.getCurrentItem() - 1);
         } else if (i == R.id.ivNext && !isInLastPage) {
             vpOnboarderPager.setCurrentItem(vpOnboarderPager.getCurrentItem() + 1);
+        } else if(i == R.id.textViewSkip) {
+            onSkipButtonPressed();
         }
     }
 
@@ -135,10 +139,12 @@ public abstract class AhoyOnboarderActivity extends AppCompatActivity implements
             fadeOut(ivPrev);
             fadeIn(ivNext);
             hideFinish();
+            textViewSkipOnBoarding.setVisibility(View.VISIBLE);
             fadeIn(circleIndicatorView);
         } else {
             fadeIn(circleIndicatorView);
             hideFinish();
+            textViewSkipOnBoarding.setVisibility(View.VISIBLE);
             fadeIn(ivPrev);
             fadeIn(ivNext);
         }
@@ -146,6 +152,8 @@ public abstract class AhoyOnboarderActivity extends AppCompatActivity implements
         if (solidBackground && (pages.size() == colorList.size())) {
             backgroundImage.setBackgroundColor(ContextCompat.getColor(this, colorList.get(position)));
         }
+
+        onboardCardChanges(position);
 
     }
 
@@ -218,6 +226,7 @@ public abstract class AhoyOnboarderActivity extends AppCompatActivity implements
 
     private void showFinish() {
         btnSkip.setVisibility(View.VISIBLE);
+        textViewSkipOnBoarding.setVisibility(View.GONE);
         this.btnSkip.animate().translationY(0 - dpToPixels(5, this)).setInterpolator(new DecelerateInterpolator()).setDuration(500).start();
     }
 
@@ -236,7 +245,6 @@ public abstract class AhoyOnboarderActivity extends AppCompatActivity implements
 
             @Override
             public void onAnimationEnd(Animator animator) {
-
                 btnSkip.setVisibility(View.VISIBLE);
             }
 
@@ -263,6 +271,8 @@ public abstract class AhoyOnboarderActivity extends AppCompatActivity implements
     }
 
     abstract public void onFinishButtonPressed();
+    abstract public void onSkipButtonPressed();
+    abstract public void onboardCardChanges(int toPosition);
 
     public void showNavigationControls(boolean navigation) {
         if (navigation) {
@@ -310,6 +320,14 @@ public abstract class AhoyOnboarderActivity extends AppCompatActivity implements
 
     public void setActiveIndicatorColor(int color) {
         this.circleIndicatorView.setActiveIndicatorColor(color);
+    }
+
+    public void setTextSkipButton(String text) {
+        this.textViewSkipOnBoarding.setText(text);
+    }
+
+    public void setColorTextSkipButton(int color) {
+        this.textViewSkipOnBoarding.setTextColor(color);
     }
 
     /**
